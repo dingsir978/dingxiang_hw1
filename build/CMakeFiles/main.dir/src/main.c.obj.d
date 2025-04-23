716db15234a4c9da41c3429bd06404638f9cc4fe
dingsir978 <3240101589@zju.edu.cn>
2025-04-23 16:53:18 +0800

    init commit

---
+++ build/CMakeFiles/main.dir/src/main.c.obj.d
@@ -0,0 +1,15 @@
+CMakeFiles/main.dir/src/main.c.obj: D:\xlab\dingxiang_hw1-main\src\main.c \
+ D:/devc++/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/stdio.h \
+ D:/devc++/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/crtdefs.h \
+ D:/devc++/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/_mingw.h \
+ D:/devc++/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ D:/devc++/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ D:/devc++/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/vadefs.h \
+ D:/devc++/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ D:/devc++/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ D:/devc++/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ D:/devc++/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ D:/devc++/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/swprintf.inl \
+ D:/devc++/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ D:/devc++/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/_mingw_print_pop.h \
+ D:/xlab/dingxiang_hw1-main/inc/algebra.h
